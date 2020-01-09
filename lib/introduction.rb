911e675e44f01d63b965da7e6647f78f1507918a
Sonata Arrington <sarrington1009@gmail.com>
2020-01-09 00:32:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,9 @@
-def introduction(name, language)
- puts "Hi, my name is #{name} and I am learning #{language}."
+def introduction(name)
+ puts "Hi, my name is #{name}."
 end 
 
-
+def introduction_with_language(name, language)
+  puts
 
 
 
